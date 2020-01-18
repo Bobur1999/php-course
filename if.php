5eb29d7fba6a4b852ebe936513c4eef8092a6d04
Bobur <Boburbekrustamov1999@gmail.com>
2020-01-18 10:10:56 +0500

    First lesson

---
+++ if.php
@@ -0,0 +1,31 @@
+<?php
+    $myInt = 89;
+    $myBool = false;
+    //Classical if
+    if ($myInt > 90) {
+        //Here some code
+    }
+    else if ($myBool == true) {
+        //Here some code
+    }
+    else {
+        //Here some code
+        echo 'bvbvbvbvbvbvbvb';
+    }
+
+    //Binary if
+    // $myVar = ($myInt >= 90) ? 'Katta' : 'Kichik';
+    
+
+    /**
+     * AND <-> &&
+     * OR <-> ||
+     * NOT <-> !
+     * ==
+     * != => not
+     * >=
+     * <=
+     * >
+     * <
+     * === - tip va qiymat tengligini solishtirish
+     */
